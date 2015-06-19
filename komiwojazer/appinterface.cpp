20679ebed4798ff4eafa92754f891a892437bb1d
magdamagdamagda <magdalena.malenda@gmail.com>
2015-06-19 22:37:30 +0200

    intrefaces

---
+++ komiwojazer/appinterface.cpp
@@ -0,0 +1,5 @@
+#include "appinterface.h"
+
+AppInterface::AppInterface()
+{
+}
---
+++ komiwojazer/appinterface.h
@@ -0,0 +1,10 @@
+#ifndef APPINTERFACE_H
+#define APPINTERFACE_H
+
+class AppInterface
+{
+public:
+    AppInterface();
+};
+
+#endif // APPINTERFACE_H
